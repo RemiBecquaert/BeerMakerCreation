6329a58a8b9a29e6c60d99859b4a3a097f54314f
Rémi <remi.becquaert35@gmail.com>
2021-10-04 16:14:09 +0200

    add serialization

--- beerMaker2/app/src/main/java/com/example/beermaker/Activity2.java
+++ beerMaker2/app/src/main/java/com/example/beermaker/Activity2.java
@@ -2,6 +2,8 @@ package com.example.beermaker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.view.inputmethod.InputMethodManager;
 
 import java.util.ArrayList;
 
@@ -26,6 +29,8 @@ public class Activity2 extends AppCompatActivity {
     private TextView volEauRinc;
     private TextView quantiteLevure;
     private LinearLayout resultats;
+
+    private String nomFichierSerialise = "recette";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +55,7 @@ public class Activity2 extends AppCompatActivity {
         quantiteLevure = findViewById(R.id.quantiteLevure);
         volEauBrass = findViewById(R.id.volEauBrass);
         volEauRinc = findViewById(R.id.volEauRinc);
+        checkSerialize();
         recette = new Recette();
     }
     private void calculBiere(){
@@ -66,6 +72,23 @@ public class Activity2 extends AppCompatActivity {
         volEauBrass.setText("Volume d'eau de brassage : " + recette.quantiteEauBrassage() + " L");
         quantiteLevure.setText("Quantité de levure : " + recette.calculLevure() + " g");
     }
+
+    private void recupSerialize(Context contexte){
+        recette = (Recette) Serializer.deserialize(nomFichierSerialise, contexte);
+    }
+
+    private void checkSerialize(){
+        try{
+            recupSerialize(this);
+            ((TextView) findViewById(R.id.moyenneEBC)).setText("" + recette.getEbc());
+            ((TextView) findViewById(R.id.degreAlcool)).setText("" + recette.getDegreB());
+            ((TextView) findViewById(R.id.volumeBiere)).setText("" + recette.getVolumeB());
+
+
+        }catch (Exception e){
+
+        }
+    }
     private void ecouteBtnClose(){
         findViewById(R.id.buttonClose).setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
@@ -77,9 +100,24 @@ public class Activity2 extends AppCompatActivity {
     private void ecouteBtnCalculer(){
         findViewById(R.id.buttonCalculer).setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+
                 calculBiere();
+                Serializer.serialize(nomFichierSerialise, recette, Activity2.this);
+
+                hideKeyboard(Activity2.this);
             }
         });
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 }
